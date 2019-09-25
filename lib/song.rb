92ff88aea77bd9cfb01605ab6212444cfb0f4ae4
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-25 20:18:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,12 @@ class Song
 
   attr_accessor :name, :artist, :genre
   @@count = 0
-  @@artist = []
+  @@artists = []
   @@genre = []
 
   def initialize(name, artist, genre)
     @@name = name
-    @@artist = artist
+    @@artists = artist
     @@genre = genre
     @@count += 1
   end
@@ -25,7 +25,7 @@ class Song
   end
 
   def self.artist_count
-    @@artist_count
+
   end
 
 
