7c3d0a37a55d68b2b928dc583bb10eb81bb19d3c
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-25 20:00:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,18 @@
 class Song
 
-attr_accessor :name, :artist, :genre
-@@count = 0
-@@artist = []
-@@genre = []
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artist = []
+  @@genre = []
 
-def intialize(artist, genre, name)
- @@artist = artist
- @@genre = genre
- @@count += 1
-end
+  def intialize(artist, genre, name)
+    @@artist = artist
+    @@genre = genre
+    @@count += 1
+  end
 
-def self.count
-  @@count
+  def self.count
+    @@count
+  end
+
 end
